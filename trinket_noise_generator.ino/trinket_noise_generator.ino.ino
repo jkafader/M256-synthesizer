eface905c659b063742aa764f063729adf09b729
James Kafader <jkafader@archive.org>
2018-04-24 13:53:53 -0700

    added trinket noise generator code... this is under test.

---
+++ trinket_noise_generator.ino/trinket_noise_generator.ino.ino
@@ -0,0 +1,16 @@
+#define SPK_PIN 1
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SPK_PIN, OUTPUT);
+  randomSeed(analogRead(2));
+}
+
+void loop() {
+   int output = random(0,2);
+   digitalWrite (SPK_PIN, output & 1);
+   delay(10);
+   output = random(0,2);
+   digitalWrite (SPK_PIN, output & 1);
+  // put your main code here, to run repeatedly:
+}
